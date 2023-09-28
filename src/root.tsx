47d4797f22ab9b24f3cfd74ac0637d96bbea91ba
pengzhanbo <volodymyr@foxmail.com>
2023-09-29 01:49:35 +0800

    feat: assets & entry

---
+++ src/root.tsx
@@ -0,0 +1,46 @@
+// @refresh reload
+import '~/styles'
+import { useAssets } from 'solid-js/web'
+import {
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from 'solid-start'
+import { type StyleData, StyleRegistry, renderSheets } from 'solid-styled'
+import Layout from './Layout'
+
+export default function Root() {
+  const sheets: StyleData[] = []
+  useAssets(() => renderSheets(sheets))
+
+  return (
+    <StyleRegistry styles={sheets}>
+      <Html lang="zh-CN">
+        <Head>
+          <Title>What Up Music</Title>
+          <Meta charset="utf-8" />
+          <Meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <Body>
+          <ErrorBoundary>
+            <Layout>
+              <Routes>
+                <FileRoutes />
+              </Routes>
+            </Layout>
+          </ErrorBoundary>
+          <Scripts />
+        </Body>
+      </Html>
+    </StyleRegistry>
+  )
+}
