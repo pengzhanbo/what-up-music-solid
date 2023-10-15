2c00ff06710108112ea6c5d541df25d9d425e47e
pengzhanbo <volodymyr@foxmail.com>
2023-10-16 00:21:35 +0800

    wip: api play url

--- src/apis/song/play-url.ts
+++ src/apis/song/play-url.ts
@@ -7,10 +7,18 @@ import type { SongPlayUrl } from '~/typing/song'
 export const getSongPlayUrl = post<
   {
     id: string | number
-    br?: number
+    level:
+      | 'standard'
+      | 'higher'
+      | 'exhigh'
+      | 'lossless'
+      | 'hires'
+      | 'jyeffect'
+      | 'sky'
+      | 'jymaster'
   },
   GetSongPlayUrlResponse
->('/song/url')
+>('/song/url/v1')
 
 export interface GetSongPlayUrlResponse {
   code: number
