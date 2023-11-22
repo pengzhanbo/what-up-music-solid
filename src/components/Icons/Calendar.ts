84ece10e6a166b3836c802d5c3fee0a88f94d4e1
pengzhanbo <volodymyr@foxmail.com>
2023-11-22 08:27:23 +0800

    feat: add Calendar date icon

---
+++ src/components/Icons/Calendar.ts
@@ -0,0 +1,99 @@
+import gen from './IconInternal'
+
+import c1 from '~icons/arcticons/calendar-1'
+import c10 from '~icons/arcticons/calendar-10'
+import c11 from '~icons/arcticons/calendar-11'
+import c12 from '~icons/arcticons/calendar-12'
+import c13 from '~icons/arcticons/calendar-13'
+import c14 from '~icons/arcticons/calendar-14'
+import c15 from '~icons/arcticons/calendar-15'
+import c16 from '~icons/arcticons/calendar-16'
+import c17 from '~icons/arcticons/calendar-17'
+import c18 from '~icons/arcticons/calendar-18'
+import c19 from '~icons/arcticons/calendar-19'
+import c2 from '~icons/arcticons/calendar-2'
+import c20 from '~icons/arcticons/calendar-20'
+import c21 from '~icons/arcticons/calendar-21'
+import c22 from '~icons/arcticons/calendar-22'
+import c23 from '~icons/arcticons/calendar-23'
+import c24 from '~icons/arcticons/calendar-24'
+import c25 from '~icons/arcticons/calendar-25'
+import c26 from '~icons/arcticons/calendar-26'
+import c27 from '~icons/arcticons/calendar-27'
+import c28 from '~icons/arcticons/calendar-28'
+import c29 from '~icons/arcticons/calendar-29'
+import c3 from '~icons/arcticons/calendar-3'
+import c30 from '~icons/arcticons/calendar-30'
+import c31 from '~icons/arcticons/calendar-31'
+import c4 from '~icons/arcticons/calendar-4'
+import c5 from '~icons/arcticons/calendar-5'
+import c6 from '~icons/arcticons/calendar-6'
+import c7 from '~icons/arcticons/calendar-7'
+import c8 from '~icons/arcticons/calendar-8'
+import c9 from '~icons/arcticons/calendar-9'
+
+export const IconCalendar1 = gen(c1)
+export const IconCalendar2 = gen(c2)
+export const IconCalendar3 = gen(c3)
+export const IconCalendar4 = gen(c4)
+export const IconCalendar5 = gen(c5)
+export const IconCalendar6 = gen(c6)
+export const IconCalendar7 = gen(c7)
+export const IconCalendar8 = gen(c8)
+export const IconCalendar9 = gen(c9)
+export const IconCalendar10 = gen(c10)
+export const IconCalendar11 = gen(c11)
+export const IconCalendar12 = gen(c12)
+export const IconCalendar13 = gen(c13)
+export const IconCalendar14 = gen(c14)
+export const IconCalendar15 = gen(c15)
+export const IconCalendar16 = gen(c16)
+export const IconCalendar17 = gen(c17)
+export const IconCalendar18 = gen(c18)
+export const IconCalendar19 = gen(c19)
+export const IconCalendar20 = gen(c20)
+export const IconCalendar21 = gen(c21)
+export const IconCalendar22 = gen(c22)
+export const IconCalendar23 = gen(c23)
+export const IconCalendar24 = gen(c24)
+export const IconCalendar25 = gen(c25)
+export const IconCalendar26 = gen(c26)
+export const IconCalendar27 = gen(c27)
+export const IconCalendar28 = gen(c28)
+export const IconCalendar29 = gen(c29)
+export const IconCalendar30 = gen(c30)
+export const IconCalendar31 = gen(c31)
+
+export const IconCalendars: Record<number, typeof IconCalendar1> = {
+  1: IconCalendar1,
+  2: IconCalendar2,
+  3: IconCalendar3,
+  4: IconCalendar4,
+  5: IconCalendar5,
+  6: IconCalendar6,
+  7: IconCalendar7,
+  8: IconCalendar8,
+  9: IconCalendar9,
+  10: IconCalendar10,
+  11: IconCalendar11,
+  12: IconCalendar12,
+  13: IconCalendar13,
+  14: IconCalendar14,
+  15: IconCalendar15,
+  16: IconCalendar16,
+  17: IconCalendar17,
+  18: IconCalendar18,
+  19: IconCalendar19,
+  20: IconCalendar20,
+  21: IconCalendar21,
+  22: IconCalendar22,
+  23: IconCalendar23,
+  24: IconCalendar24,
+  25: IconCalendar25,
+  26: IconCalendar26,
+  27: IconCalendar27,
+  28: IconCalendar28,
+  29: IconCalendar29,
+  30: IconCalendar30,
+  31: IconCalendar31,
+}
